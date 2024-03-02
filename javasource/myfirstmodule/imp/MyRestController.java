1caa08e8d0a347253867cae1bf7d890e64047051
engalar <engalar@gmail.com>
2024-03-02 14:30:35 +0800

    使用mx jetty运行时替换spring内置的servlet

--- javasource/myfirstmodule/imp/MyRestController.java
+++ javasource/myfirstmodule/imp/MyRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/spring")
 public class MyRestController {
 
     private static final ILogNode logger = Core.getLogger("MyRestService");
